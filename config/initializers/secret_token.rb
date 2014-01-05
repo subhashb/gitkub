b50f321da6b27421fd73866bae2838cebc80e21c
Subhash Bhushan <subhash.bhushan@kaybus.com>
2014-01-05 18:06:34 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitkub::Application.config.secret_key_base = '846a545ab3e1d1081bb75e1fb3d4b95f98ba0130c9df1b89cdd6576142a4819f1362676b8b7a7e36706e79789729091317efab943e7f9abff8cf1ac14983ac86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitkub::Application.config.session_store :cookie_store, key: '_gitkub_session'
